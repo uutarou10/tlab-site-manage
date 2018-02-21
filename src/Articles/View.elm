236a518a4a7cc6cc88b6a2c6ed3870119868d908
Kota Nonaka <kota.kaicho@gmail.com>
2018-02-21 14:22:03 +0900

    Update articles view.

--- src/Articles/View.elm
+++ src/Articles/View.elm
@@ -2,24 +2,31 @@ module Articles.View exposing (..)
 
 import Articles.Messages exposing (Msg(..))
 import Articles.Model exposing (Article, Model)
-import Html exposing (Html, div, table, tbody, td, text, th, thead, tr)
+import Html exposing (Html, div, table, tbody, td, text, th, thead, tr, button, a)
+import Html.Attributes exposing (class, href)
 
 
 view : Model -> Html Msg
 view articles =
-    table []
-        [ thead []
-            [ tr []
-                [ th []
-                    [ text "記事タイトル" ]
-                , th []
-                    [ text "作成者" ]
-                , th []
-                    [ text "作成日時" ]
-                ]
+    div []
+        [ div [ class "btns" ]
+            [ a [ class "btn is-plain is-round is-primary new-btn" ] [ text "新規作成" ]
             ]
-        , tbody []
-            (List.map row articles)
+        , table [ class "table is-boarder is-middle" ]
+            [ thead []
+                [ tr []
+                    [ th []
+                        [ text "記事タイトル" ]
+                    , th []
+                        [ text "作成者" ]
+                    , th []
+                        [ text "作成日時" ]
+                    , th [] []
+                    ]
+                ]
+            , tbody []
+                (List.map row articles)
+            ]
         ]
 
 
@@ -32,4 +39,8 @@ row article =
             [ text <| toString article.user_id ]
         , td []
             [ text article.created_at ]
+        , td [ class "btns is-center" ]
+            [ a [ class "btn is-plain is-round is-primary", href <| "#/articles/" ++ (toString article.id) ] [ text "編集" ]
+            , a [ class "btn is-plain is-round is-danger", href "#" ] [ text "削除" ]
+            ]
         ]
